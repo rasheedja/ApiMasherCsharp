1fa4f15cb34a838882ce17771d78d2cefa074954
Junaid Ali Rasheed <jrasheed178@gmail.com>
2016-03-10 22:12:51 +0000

    Added documentation for the Program class.

--- ApiMasherCsharp/Program.cs
+++ ApiMasherCsharp/Program.cs
@@ -9,6 +9,9 @@ namespace ApiMasherCsharp
 {
     class Program
     {
+        /// <summary>
+        /// Sets up the parameters to use for the API in an ArrayList.
+        /// </summary>
         static void Main(string[] args)
         {
 
@@ -17,28 +20,28 @@ namespace ApiMasherCsharp
             GuardinCategories.Add("Uk");
             GuardinCategories.Add("World");
             GuardinCategories.Add("Sport");
-              GuardinCategories.Add("Football");
-              GuardinCategories.Add("Life&style");
-              GuardinCategories.Add("Culture");
-              GuardinCategories.Add("Business");
-              GuardinCategories.Add("Travel");
-              GuardinCategories.Add("Technology");
-              GuardinCategories.Add("Travel");
-              GuardinCategories.Add("Environment");
+            GuardinCategories.Add("Football");
+            GuardinCategories.Add("Life&style");
+            GuardinCategories.Add("Culture");
+            GuardinCategories.Add("Business");
+            GuardinCategories.Add("Travel");
+            GuardinCategories.Add("Technology");
+            GuardinCategories.Add("Travel");
+            GuardinCategories.Add("Environment");
 
-              foreach (string Category in GuardinCategories) {
-                  System.Threading.Thread.Sleep(1000); // pause for second !Important do not remove even for testing
-                  // max 12 calls per second, or we will get banned
-                  // max 5000 total calls per month
-                  // Check Monthly Tally if in doubt
-                  //http://explorer.content.guardianapis.com/#/search?q=Uk&show-fields=all&date-id=date%2Flast24hours&api-key=
-                  // from above URL check the   "total": in the response field
-
-                  dynamic Result = GuardianPost.DoGet("http://content.guardianapis.com/search?q=" + Category + "&show-fields=all&date-id=date%2Flast24hours&api-key=);
+            foreach (string Category in GuardinCategories) {
+                System.Threading.Thread.Sleep(1000); // pause for second !Important do not remove even for testing
+                // max 12 calls per second, or we will get banned
+                // max 5000 total calls per month
+                // Check Monthly Tally if in doubt
+                // http://explorer.content.guardianapis.com/#/search?q=Uk&show-fields=all&date-id=date%2Flast24hours&api-key=
+                // from above URL check the   "total": in the response field
+                
+                dynamic Result = GuardianPost.DoGet("http://content.guardianapis.com/search?q=" + Category + "&show-fields=all&date-id=date%2Flast24hours&api-key=);
               
               };
-
-
+        
+         
         }
     }
 }
